9e4692b364538572a79b4f9b74109ae1bcca7942
Mariel de la Garza <mariel.delagarza@viget.com>
2023-10-11 13:49:41 -0400

    add more repos

---
+++ SQL-bear-organizer-lab-online-web-pt-112618/lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 1, 'M', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 1, 'M', 'black', 'hyper', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 2, 'F', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 2, 'M', 'black', 'grumpy', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 3, 'F', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 3, 'F', 'black', 'grumpy', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 4, 'F', 'brown', 'calm', 1);
+INSERT INTO bears VALUES (8, NULL, 4, 'M', 'black', 'grumpy', 0);
